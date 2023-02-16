ff4c1d58c2962dd500a8d80423924e3b74380c86
Mdsimultaneous <abdullahisaiduabdullahi2003@gmail.com>
2023-02-16 15:58:07 +0000

    fdsfdsfsd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A programming low leve
- *Return 0 (Success)
+ * main - A programming that prints the size of various computer types
+ * Return 0 (Success)
  */
 int main(void)
 {
