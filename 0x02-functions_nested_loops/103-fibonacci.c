d2004de34d6762727e129bf3a2c80340247876c7
Mdsimultaneous <abdullahisaiduabdullahi2003@gmail.com>
2023-02-21 23:59:19 +0100

    gahauaju

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - jwjahahwjja
+ * less than 4000000
+ * Return: Nothimg!
+ */
+
+int main(void)
+
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+	while loop (k + j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	++i;
+	}
+	printf("%ld\n", sum);
+	retturn (0);
+}
