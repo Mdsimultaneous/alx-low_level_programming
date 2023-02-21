b0f085a00948c98131eb31694233cedab087618d
Mdsimultaneous <abdullahisaiduabdullahi2003@gmail.com>
2023-02-21 16:45:32 +0100

    hagajah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <main.h>
 /**
  * main - prints Holberton as a message
  * Return: Always 0 (Success)
@@ -10,7 +10,7 @@ int main(void)
 	sz=sizeof(st)/sizeof(int);
 	for(count=0; count<sz; count++)
 	{
-		_putchar(str[count]);
+		_putchar(str(count));
 	}
 	_putchar(/n);
 	return(0);
