103752266e1fb3e9a4e7523470b4dc7379ed8888
Mdsimultaneous <abdullahisaiduabdullahi2003@gmail.com>
2023-02-21 21:55:06 +0100

    gagwhga

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putcahr('a')
-	_putchat('r')
-	_putchar('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putcahr('a');
+	_putchat('r');
+	_putchar('\n');
 	return (0);
 }
