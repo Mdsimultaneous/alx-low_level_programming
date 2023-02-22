ab8e79e2e29e72b4c9fd836fdafbdcebbfecf463
Mdsimultaneous <abdullahisaiduabdullahi2003@gmail.com>
2023-02-22 12:12:50 +0100

    jajwjwj

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -13,3 +13,15 @@ int _abs(int n)
 	}
 	return (-n);
 }
+int _abs(int a)
+{
+    int k = a;
+    int absvalue;
+
+    if (k < 0)
+    {
+        k = k * (-1);
+    }
+    absvalue = k;
+    return (absvalue);
+}
