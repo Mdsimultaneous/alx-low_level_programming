791dc5b339ad81f5c694e6261df4b559eb036591
Mdsimultaneous <abdullahisaiduabdullahi2003@gmail.com>
2023-02-22 12:23:54 +0100

    jajajaj

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,10 +5,17 @@
 *Return:returns the abs value.
 *
 */
-int _abs(int n)
+int main(void)
 {
-	if (n >= 0)
-		return (n);
-	else
-		return (-n);
+    int r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    return (0);
 }
